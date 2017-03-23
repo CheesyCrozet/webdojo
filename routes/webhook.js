3dfe3f58c3e3e800b8821c9ea3454ecd89035e2d
Theo Barollet <theo.barollet@aliceadsl.fr>
2017-03-23 15:31:51 +0100

    connected ?

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,71 +2,73 @@ var express = require('express');
 var router = express.Router();
 
 const
-  chatService = require('../server/chatService'),
-  weatherService = require('../server/weatherService'),
-  WeatherData = require('../server/model/weatherData'),
-  userService = require('../server/userService'),
-  parser = require('json-parser');
+chatService = require('../server/chatService'),
+weatherService = require('../server/weatherService'),
+WeatherData = require('../server/model/weatherData'),
+userService = require('../server/userService'),
+parser = require('json-parser');
 
 /* GET webhook auth. */
 router.get('/', function(req, res, next) {
-  if (chatService.authenticate(req)) {
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(403);
-  };
+    if (chatService.authenticate(req)) {
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(403);
+    };
 });
 
 router.get('/webhook', function(req, res) {
 });
 
 router.post('/webhook', function(req, res) {
-    var data = req.body;
+    chatService.authenticate(res);
+    /*var data = req.body;
 
     if(data.object === 'page') {
-	console.log("ya un truc");
-	data.entry.forEach(function(entry) {
-	    var pageID = entry.id;
-	    var timeOfEvent = entry.time;
+        console.log("ya un truc");
+        data.entry.forEach(function(entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
 
-	    entry.messaging.forEach(function(event) {
-		if(event.message) {
-		    receivedMessage(event);
-		} else {
-			console.log("Webhook unknow event", event);
-		}
-	    });
-	}
-	req.sendStatus(200);
-    }
+            entry.messaging.forEach(function(event) {
+                if(event.message) {
+                    receivedMessage(event);
+                } else {
+                    console.log("Webhook unknow event", event);
+                }
+            });
+        }
+        req.sendStatus(200);
+    }*/
+
 });
 
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-	   senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+    console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
+    var messageId = message.mid;
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
 
-  if (messageText) {
-    switch (messageText) {
-	    case 'generic':
-	        sendGenericMessage(senderID);
-	        break;
-      default:
-        sendTextMessage(senderID, messageText);
+    if (messageText) {
+        switch (messageText) {
+            case 'generic':
+            sendGenericMessage(senderID);
+            break;
+            default:
+            sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
     }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
 
 /* POST route for receiving message */
